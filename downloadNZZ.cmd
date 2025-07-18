4c7c144baa7ce838a5ce1d7225d7439cf1feed81
Michael Moser <17732576+mmoser18@users.noreply.github.com>
2025-07-18 21:56:55 +0200

    adapted cmd to new version

--- downloadNZZ.cmd
+++ downloadNZZ.cmd
@@ -4,7 +4,7 @@ rem Released under GPL V3 or later
 echo running NZZ downloader:
 rem program executable location:
 set loc=%HOME%\Documents\eclipse\workspace\nzz_download\target\
-set jar=%loc%\nzz_download-1.2.0.jar
+set jar=%loc%nzz_download-1.3.0.jar
 rem download location:
 set tgt=Y:\Things to read\NZZ\
 cd %loc%
